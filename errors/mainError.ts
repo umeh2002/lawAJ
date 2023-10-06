4e8842972e24247f8cd6d6a8c0cb87ab01276d8c
UchennaAustine <uchennaaustine8@gmail.com>
2023-10-06 13:51:01 +0100

    add

--- errors/mainError.ts
+++ errors/mainError.ts
@@ -1,35 +1,36 @@
 export enum HTTP {
-    OK = 200,
-    CREATE = 201,
-    BAD = 404
+  OK = 200,
+  CREATE = 201,
+  NOT_FOUND = 404,
+  BAD = 400,
 }
 
 interface iError {
-    name : string;
-    message : string;
-    status : HTTP;
-    success : boolean;
+  name: string;
+  message: string;
+  status: HTTP;
+  success: boolean;
 }
 
 export class mainError extends Error {
-    public readonly name : string;
-    public readonly message : string;
-    public readonly status : HTTP;
-    public readonly success : boolean = false;
+  public readonly name: string;
+  public readonly message: string;
+  public readonly status: HTTP;
+  public readonly success: boolean = false;
 
-    constructor(args : iError){
-        super(args.message )
+  constructor(args: iError) {
+    super(args.message);
 
-        Object.setPrototypeOf(this, new.target.prototype )
+    Object.setPrototypeOf(this, new.target.prototype);
 
-        this.name = args.name
-        this.message = args.message
-        this.status = args.status
+    this.name = args.name;
+    this.message = args.message;
+    this.status = args.status;
 
-        if(this.success !== undefined){
-            this.success = args.success
-        }
-
-        Error.captureStackTrace(this)
+    if (this.success !== undefined) {
+      this.success = args.success;
     }
+
+    Error.captureStackTrace(this);
+  }
 }
